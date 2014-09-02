db21eabbf01080f903d17293afcfac3fee13e530
Ivan Ivic <tyrion85@gmail.com>
2014-09-02 22:39:32 +0200

    Changed names of some functions in Set1/Challenge3

--- src/matasano_crypto_challenges/set1/challenge3.clj
+++ src/matasano_crypto_challenges/set1/challenge3.clj
@@ -2,40 +2,48 @@
   (:use [matasano-crypto-challenges.set1.challenge1]
         [matasano-crypto-challenges.set1.challenge2]))
 
-(defn- single-char-xor [hex c-key]
-  (map bit-xor (unhexify->bytes hex) (.getBytes c-key)))
+(defn- decipher-single-char [char-hex c-key]
+  "Does a 1 bit XOR on hexed character with a given key."
+   (map bit-xor (unhexify->bytes char-hex) (.getBytes c-key)))
 
-(defn- string->char [singleCharacterinString]
-  (.charAt singleCharacterinString 0))
-
-(defn- score-character [character]
-  (if (Character/isLetter character) 1 0))
-
-(defn- score-plaintext [plaintext]
-  (reduce + (map #(score-character %) (char-array plaintext))))
+(defn decipher [hex c-key]
+  "Deciphers given hex cipher with key."
+    (apply str
+         (map #(String. (unhexify->bytes
+                         (hexify->string
+                          (decipher-single-char % c-key)))
+                "UTF-8")
+              (partition 2 hex))))
 
 (defn- alphabet []
   (let [all-letters '(\a \b \c \d \e \f \g \h \i \j \k \l \m \n \o \p \q \r \s \t \u \v \w \x \y \z)]
     (distinct (concat all-letters (map #(Character/toUpperCase %) all-letters)))))
 
-(defn- all-plaintexts [hex]
+(defn- all-possible-deciphers [hex]
+  "Returns a map of all possible decipers and appropriate decipher keys."
   (apply merge
-         (map #(hash-map (single-byte-xor-cipher-with-key hex (str %)) %)
+         (map #(hash-map (decipher hex (str %)) %)
                    (alphabet))))
 
-(defn- score-all [hex]
-  (let [all (all-plaintexts hex)]
+(defn- score-character [character]
+  "Scores a character: if Letter => 1 point, else 0 points."
+   (if (Character/isLetter character) 1 0))
+
+(defn- score-plaintext [plaintext]
+  "Scores a plaintext."
+   (reduce + (map #(score-character %) (char-array plaintext))))
+
+(defn- score-all-combinations [hex]
+  "Returns a map of all scores for possible decipher keys"
+  (let [all (all-possible-deciphers hex)]
     (apply merge (map #(hash-map (score-plaintext %) (get all %)) (keys all)))))
 
-(defn single-byte-xor-cipher-with-key [hex c-key]
-  (apply str
-         (map #(String.
-                (unhexify->bytes (hexify->string (single-char-xor % c-key)))
-                "UTF-8")
-              (partition 2 hex))))
 
 (defn find-key [hex]
-  (let [all-scores (score-all hex)]
+  "Finds a most likely decipher key by 'scoring' all possible deciphers.
+  \nScoring method is straightforward: each letter is awarded one point,
+  other characters get a zero. Key with most points wins."
+  (let [all-scores (score-all-combinations hex)]
     (str
      (get all-scores
           (apply max (keys all-scores))))))
--- test/matasano_crypto_challenges/set1/challenge3_test.clj
+++ test/matasano_crypto_challenges/set1/challenge3_test.clj
@@ -2,11 +2,11 @@
   (:use midje.sweet matasano-crypto-challenges.set1.challenge3))
 
 (fact "Single byte xor function should take a hex value and a single-character key (as String) and return XORed result"
-      (single-byte-xor-cipher-with-key "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736" "a")
+      (decipher "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736" "a")
       => "zVVRPW^tzJUPR\\XIVLW]V_[XZVW")
 
 (fact "Going manually through English letters, letter 'X' gives the most sane result"
-      (single-byte-xor-cipher-with-key "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736" "X")
+      (decipher "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736" "X")
       => "Cooking MC's like a pound of bacon")
 
 (fact "Finding key by scoring each plaintext should result in letter 'X'"
